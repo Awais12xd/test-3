c4a77575314e88879d6fc1c7b06f8f76f1c36bc0
Awais12xd <lucifer.awaisxx213@example.com>
2025-02-15 12:00:19 +0500

    Update FeatureProjectCard.tsx

--- components/ui/FeatureProjectCard.tsx
+++ components/ui/FeatureProjectCard.tsx
@@ -25,7 +25,7 @@ const FeatureProjectCard = ({ project, index }: FeatureProductProps) => {
   return (
     <div
       className="relative project-layer md:bg-none bg-cover bg-center md:p-0 px-6 py-7"
-      style={{ backgroundImage: `url(@/public/images/projects/project${index + 1}.PNG)` }}
+      // style={{ backgroundImage: `url(@/public/images/projects/project${index + 1}.PNG)` }}
     >
       <Link href={project.webUrl} legacyBehavior>
         <a target="_blank" className="md:block hidden">
